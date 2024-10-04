4a9a3a70c3344c627e9d6c3f6508d854829967e9
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2024-10-04 14:45:52 +0200

    change config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-// Check if the environment is production (i.e., GitHub Pages)
+
 const isGitHubPages = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  site: isGitHubPages ? 'https://jurilorenz.github.io/maxwapp' : 'http://localhost:3000',
-  base: isGitHubPages ? '/maxwapp/' : '/',  // Use the base path only for production
-  build: {
+  site: isGitHubPages ? 'https://jurilorenz.github.io' : 'http://localhost:3000',
+  base: '/',
+    build: {
     outDir: 'dist',
   },
   integrations: [
